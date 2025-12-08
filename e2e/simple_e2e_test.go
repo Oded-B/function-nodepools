859649264d0144af78f1ed1f3ceaa6542f003996
Oded Ben Ozer <oded.benozer@coralogix.com>
2025-12-08 14:25:28 +0100

    Fix path issue Remove unused functions exit  script on first failure

--- e2e/simple_e2e_test.go
+++ e2e/simple_e2e_test.go
@@ -224,45 +224,6 @@ func TestMain(m *testing.M) {
 }
 
 // installCrossplane installs Crossplane using Helm via e2e framework
-func installCrossplane(ctx context.Context, cfg *envconf.Config) (context.Context, error) {
-	// Create Helm manager using the e2e framework
-	manager := helm.New(cfg.KubeconfigFile())
-
-	// Add Crossplane Helm repository
-	err := manager.RunRepo(helm.WithArgs("add", "crossplane-stable", "https://charts.crossplane.io/stable"))
-	if err != nil {
-		return ctx, fmt.Errorf("failed to add crossplane helm repo: %w", err)
-	}
-
-	// Update Helm repositories
-	err = manager.RunRepo(helm.WithArgs("update"))
-	if err != nil {
-		return ctx, fmt.Errorf("failed to update helm repos: %w", err)
-	}
-
-	// Install Crossplane,  TODO pin version
-	err = manager.RunInstall(
-		helm.WithName("crossplane"),
-		helm.WithNamespace("crossplane-system"),
-		helm.WithReleaseName("crossplane-stable/crossplane"),
-		helm.WithArgs("--create-namespace", "--wait", "--timeout", "10m"),
-	)
-	if err != nil {
-		return ctx, fmt.Errorf("failed to install crossplane: %w", err)
-	}
-
-	// Wait for Crossplane to be ready
-	client, err := createKubernetesClient(cfg.KubeconfigFile())
-	if err != nil {
-		return ctx, fmt.Errorf("failed to create client: %w", err)
-	}
-
-	if err := waitForDeploymentReady(client, "crossplane-system", "crossplane"); err != nil {
-		return ctx, fmt.Errorf("crossplane deployment not ready: %w", err)
-	}
-
-	return ctx, nil
-}
 
 // installKarpenter installs Karpenter using Helm via e2e framework
 func installKarpenter(ctx context.Context, cfg *envconf.Config) (context.Context, error) {
@@ -350,17 +311,6 @@ func createKubernetesClient(kubeconfig string) (*kubernetes.Clientset, error) {
 		return nil, err
 	}
 	return kubernetes.NewForConfig(config)
-}
-
-// waitForDeploymentReady waits for a deployment to be ready
-func waitForDeploymentReady(client *kubernetes.Clientset, namespace, name string) error {
-	return wait.PollUntilContextTimeout(context.Background(), 5*time.Second, 5*time.Minute, true, func(ctx context.Context) (bool, error) {
-		deployment, err := client.AppsV1().Deployments(namespace).Get(ctx, name, metav1.GetOptions{})
-		if err != nil {
-			return false, err
-		}
-		return deployment.Status.ReadyReplicas == *deployment.Spec.Replicas, nil
-	})
 }
 
 // TestCrossplaneInstallation tests that Crossplane is properly installed
